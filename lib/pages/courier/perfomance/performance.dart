80e1d9a13ac5a85d98f934d155d6f5fe8e9935a2
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2023-10-04 15:31:21 +0100

    performance

--- lib/pages/courier/perfomance/performance.dart
+++ lib/pages/courier/perfomance/performance.dart
@@ -1,3 +1,4 @@
+import 'package:app/constants.dart';
 import 'package:flutter/material.dart';
 
 class Performance extends StatefulWidget {
@@ -10,6 +11,71 @@ class Performance extends StatefulWidget {
 class _PerformanceState extends State<Performance> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tartiaryColor,
+        surfaceTintColor: tartiaryColor,
+        title: const Text("Performance"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+          children: [
+            Card(
+              color: Colors.white,
+              surfaceTintColor: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  Text("Total Orders"),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              surfaceTintColor: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  Text("Completed Orders"),
+                ],
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              surfaceTintColor: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  Text("Uncompleted Orders"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
